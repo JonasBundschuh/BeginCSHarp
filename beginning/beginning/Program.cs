3c013e14dc65d992464084460e5c2fed4f078184
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-15 14:52:40 +0200

    #27 finish - added search function.

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -6,18 +6,34 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace beginning
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //get searchValue
+            Console.WriteLine("Please enter a character you want to search for: ");
+            char searchValue = Convert.ToChar(Console.ReadLine());
+            Console.Clear();
 
+            //List of names
+            string[] names = { "Jonas", "Phillip", "Niklas", "Marcello", "Andreas", "Robin", "Robert", "Marie", "Leon", "Erwin", "Jan", "Mona", "Lukas", "Johannes" };
+            
+            //Loop
+            foreach(string name in names)
             {
 
-
-                Console.ReadLine();
+                //if name on list contains searchValue print name
+                bool valid_name = name.Contains(searchValue);
+                if(valid_name.Equals(true))
+                {
+                    Console.WriteLine(name);
+                }
             }
+            Console.ReadLine();
+
 
         }   
     }
