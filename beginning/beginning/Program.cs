2f3dd9dde44d8ffed420bdf91992fca2e8e41314
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-16 09:08:58 +0200

    #32

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -13,30 +13,42 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            //ask use how many logs he wants
-            Console.WriteLine("How many logs do you want?: ");
-            int log_amount = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
 
-            int[] numbers = new int[log_amount];
+            
          
             //get numbers from user input
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 1; i < 101; i++)
             {
-                Console.WriteLine("Give me a number for the array: ");
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                } else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                } 
+                else if (i % 3 == 0)
+                {
+                Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
 
-            //print the numbers
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.Clear();
-
-                Console.WriteLine($"Lowest Number: {numbers.Min()} ");
-                Console.WriteLine($"Highest Number: {numbers.Max()}");
-                Console.WriteLine($"Averge: {numbers.Average()}");
-            }
             Console.ReadLine();
+
+           // //print the numbers
+           // for (int i = 0; i < numbers.Length; i++)
+           // {
+           //     Console.Clear();
+           //
+           //     Console.WriteLine($"Lowest Number: {numbers.Min()} ");
+           //     Console.WriteLine($"Highest Number: {numbers.Max()}");
+           //     Console.WriteLine($"Averge: {numbers.Average()}");
+           // }
+           // Console.ReadLine();
 
             
 
