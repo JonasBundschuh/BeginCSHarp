270891b63232adf41b0fb3b8d6ff8d3ef2a685b3
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-16 08:46:15 +0200

    #31

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -15,12 +15,11 @@ namespace beginning
         {
             //ask use how many logs he wants
             Console.WriteLine("How many logs do you want?: ");
-            int log_amount = Convert.ToInt32(Console.ReadLine()); 
+            int log_amount = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
 
             int[] numbers = new int[log_amount];
-           // int[] numbers2 = new int[] { 26, 8, 12, 3, 6 };
-
-
+         
             //get numbers from user input
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -28,19 +27,15 @@ namespace beginning
                 numbers[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            
-            Console.Write("[ ");
-
-
             //print the numbers
             for (int i = 0; i < numbers.Length; i++)
             {
-                Console.Write($"{numbers[i]} | ");
+                Console.Clear();
+
+                Console.WriteLine($"Lowest Number: {numbers.Min()} ");
+                Console.WriteLine($"Highest Number: {numbers.Max()}");
+                Console.WriteLine($"Averge: {numbers.Average()}");
             }
-
-
-            Console.Write(" ]");
-
             Console.ReadLine();
 
             
