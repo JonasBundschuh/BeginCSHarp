c267592e7c8b2058c633f52e8475895f8485dde8
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-16 08:37:20 +0200

    #30

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -13,13 +13,33 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            string[] names = new string[13];
-            string[] names2 = new string[] { "Lukas", "Jonas", "Mona", "Marie", "Philip", "Jan", "Erwin", "Leon", "Marcello", "Johannes", "Robin", "Andreas", "Robert" };
+            //ask use how many logs he wants
+            Console.WriteLine("How many logs do you want?: ");
+            int log_amount = Convert.ToInt32(Console.ReadLine()); 
 
-            foreach(string trainee in names2)
+            int[] numbers = new int[log_amount];
+           // int[] numbers2 = new int[] { 26, 8, 12, 3, 6 };
+
+
+            //get numbers from user input
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(trainee);
+                Console.WriteLine("Give me a number for the array: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
             }
+
+            
+            Console.Write("[ ");
+
+
+            //print the numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} | ");
+            }
+
+
+            Console.Write(" ]");
 
             Console.ReadLine();
 
